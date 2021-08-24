2a4d4d943840664e8533ac1637ccbff50a23b51b
Roshan2810 <rosu285shah@gmail.com>
2021-08-24 20:08:08 +0530

    Product added

--- example.js
+++ example.js
@@ -1,22 +1,21 @@
-
 const data = [
   {
     name: "iPhone12",
     description: "New Apple iPhone 12 (128GB) - Blue",
     available: "yes",
-    url:"https://m.media-amazon.com/images/I/71ZOtNdaZCL._SL1500_.jpg"
+    url: "https://m.media-amazon.com/images/I/71ZOtNdaZCL._SL1500_.jpg",
   },
   {
     name: "iPhone11",
     description: "New Apple iPhone 11 (64GB) - Black",
     available: "yes",
-    url:"https://m.media-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg"
+    url: "https://m.media-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg",
   },
   {
     name: "iPhone XR",
     description: "Apple iPhone XR (64GB) - Black ",
     available: "yes",
-    url:"https://m.media-amazon.com/images/I/519KIlHA2wL._SL1024_.jpg"
+    url: "https://m.media-amazon.com/images/I/51PuFBgBK4L._SL1024_.jpg",
   },
 ];
 
