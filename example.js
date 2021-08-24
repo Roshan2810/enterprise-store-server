d457e76d4f0f5c4ec198b05d5a927520dcf2af7a
Roshan2810 <rosu285shah@gmail.com>
2021-08-24 20:04:33 +0530

    Product Id added

--- example.js
+++ example.js
@@ -1,24 +1,23 @@
-const uuidv1 = require("uuid/v1");
 
 const data = [
-    {
-        "productId": uuidv1(),
-        "name": "iPhone12",
-        "description": "",
-        "available": "yes"
-    },
-    {
-        "productId": uuidv1(),
-        "name": "iPhone11",
-        "description": "",
-        "available": "yes"
-    },
-    {
-        "productId": uuidv1(),
-        "name": "iPhone10",
-        "description": "",
-        "available": "yes"
-    }
-]
+  {
+    name: "iPhone12",
+    description: "New Apple iPhone 12 (128GB) - Blue",
+    available: "yes",
+    url:"https://m.media-amazon.com/images/I/71ZOtNdaZCL._SL1500_.jpg"
+  },
+  {
+    name: "iPhone11",
+    description: "New Apple iPhone 11 (64GB) - Black",
+    available: "yes",
+    url:"https://m.media-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg"
+  },
+  {
+    name: "iPhone XR",
+    description: "Apple iPhone XR (64GB) - Black ",
+    available: "yes",
+    url:"https://m.media-amazon.com/images/I/519KIlHA2wL._SL1024_.jpg"
+  },
+];
 
-module.exports = data
+module.exports = data;
--- server.js
+++ server.js
@@ -1,53 +1,62 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const redis = require("redis");
+const cors = require("cors");
 const app = express();
-const redis = require("redis");
 const client = redis.createClient();
-const redisData = require("./example")
-const httperror = require("http-errors")
+const redisData = require("./example");
+const httperror = require("http-errors");
+const { v4: uuidv4 } = require("uuid");
 
-// client.set(
-//   "products",
-//   JSON.stringify({redisData }),
-//   (err, reply) => {
-//     console.log(reply, err);
-//   }
-// );
+app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => next());
 
+const generateProductId = (redisData) => {
+  return redisData.map((entry) => {
+    entry.productId = uuidv4();
+    return entry;
+  });
+};
+
+client.set(
+  "products",
+  JSON.stringify(generateProductId(redisData)),
+  (err, reply) => {
+    console.log(reply, err);
+  }
+);
+
 app.get("/product/getCampaignProducts", (req, res) => {
-  try{
-  client.get("products", (err, result) => {
+  try {
+    client.get("products", (err, result) => {
       return res.send({
         success: true,
         status: 200,
-        data: result
+        data: JSON.parse(result),
       });
-  });
-}catch(err){
-  return res.send({
-    success: false,
-    message: err,
-  });
-
-}
+    });
+  } catch (err) {
+    return res.send({
+      success: false,
+      message: err,
+    });
+  }
 });
 
 app.get("/product/getProductDetails/:productId", (req, res) => {
-  try{
-  const product = req.params.productId;
-  
-  client.get("products",(err, result) => {
-    console.log(result)
-        res.send({ status: 200 ,data: JSON.parse(result)});
-  });
-  }catch(err){
-    console.log(err)
-  }
+  try {
+    const product = req.params.productId;
 
+    client.get("products", (err, result) => {
+      console.log(result);
+      res.send({ status: 200, data: JSON.parse(result) });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/product/addToCart", (req, res) => {
