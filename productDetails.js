197d3f64d05af9b8755886d13bc777ee362a34b6
Roshan2810 <rosu285shah@gmail.com>
2021-08-29 12:15:05 +0530

    Quantity change

--- productDetails.js
+++ productDetails.js
@@ -5,7 +5,7 @@ const productDetails = [
     rating: "4.0",
     mrp: "74,999",
     discountedPrice: "70,999",
-    quantity: 2,
+    quantity: 1000,
     productDesc: [
       { value: "IPhone 12", name: "Model Name" },
       { value: "Apple", name: "Brand" },
@@ -27,7 +27,7 @@ const productDetails = [
     rating: "4.5",
     mrp: "56,999",
     discountedPrice: "50,999",
-    quantity: 2,
+    quantity: 1000,
     productDesc: [
       { value: "IPhone 11", name: "Model Name" },
       { value: "Apple", name: "Brand" },
@@ -51,7 +51,7 @@ const productDetails = [
     rating: "4.0",
     mrp: "44,999",
     discountedPrice: "40,999",
-    quantity: 2,
+    quantity: 1000,
     productDesc: [
       { value: "iPhone XR", name: "Model Name" },
       { value: "Apple", name: "Brand" },
